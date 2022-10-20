15771f550c9f3a11e3cd237a989be2845a483416
alyssa225 <alyssachen2022@u.northwestern.edu>
2022-10-19 23:25:18 -0500

    added test files

---
+++ src/turtle_brick/test/brick.test.py
@@ -0,0 +1,21 @@
+from ament_copyright.main import main
+import pytest
+import launch
+import launch_pytest
+import sys
+import launch_ros
+from pathlib import Path
+#found this example here https://github.com/ros2/launch/blob/humble/launch_pytest/test/launch_pytest/examples/check_node_msgs.py
+
+# def generate_test_description():
+#     path_to_test = Path(__file__).parent
+
+#     return launch.LaunchDescription([
+#         launch_ros.actions.Node(
+#             executable=sys.executable,
+#             arguments=[str(path_to_test / 'executables' / 'talker.py')],
+#             additional_env={'PYTHONUNBUFFERED': '1'},
+#             name='demo_node_1',
+#             output='screen',
+#         ),
+#     ])
---
+++ src/turtle_brick/test/math_test.py
@@ -0,0 +1,6 @@
+class TestMath:
+    def test_math_sucsses():
+        assert 3**2 == 9
+        
+    def test_math_fail():
+        assert 5+6 == 20
