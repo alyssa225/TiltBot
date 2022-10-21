8cfb9ef72b18290eb88f5900da0bfb26e9e1829e
alyssa225 <alyssachen2022@u.northwestern.edu>
2022-10-20 19:36:31 -0500

    test files and other small fixes

--- src/turtle_brick/test/brick.test.py
+++ src/turtle_brick/test/brick.test.py
@@ -1,22 +1,68 @@
-# from ament_copyright.main import main
-# import pytest
-# import launch
-# import launch_pytest
-# import sys
-# import launch_ros
-# from pathlib import Path
-# found this example here
-# https://github.com/ros2/launch/blob/humble/launch_pytest/test/launch_pytest/examples/check_node_msgs.py
+import unittest
+from launch import LaunchDescription
+from launch_ros.actions import Node
+import launch_testing
+import pytest
+import rclpy
+from geometry_msgs.msg import PoseStamped
+from geometry_msgs.msg import Twist
 
-# def generate_test_description():
-#     path_to_test = Path(__file__).parent
 
-#     return launch.LaunchDescription([
-#         launch_ros.actions.Node(
-#             executable=sys.executable,
-#             arguments=[str(path_to_test / 'executables' / 'talker.py')],
-#             additional_env={'PYTHONUNBUFFERED': '1'},
-#             name='demo_node_1',
-#             output='screen',
-#         ),
-#     ])
+@pytest.mark.rostest
+def test_cmdvel():
+    turtle_robot_action = Node(package="turtle_brick",
+                               executable="turtle_robot", )
+    return (
+        LaunchDescription([
+            turtle_robot_action,
+            launch_testing.actions.ReadyToTest()
+            ]),
+        {
+            'turtle_robot': turtle_robot_action
+        }
+    )
+
+
+class TestTurtleRobot(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        rclpy.init()
+
+    @classmethod
+    def tearDownClass(cls):
+        rclpy.shutdown()
+
+    def setUp(self):
+        self.node = rclpy.create_node("test_node")
+
+    def tearDown(self):
+        self.node.destroy_node()
+
+    def test_publisher(self, launch_service, in_out, proc_output):
+        self.timestart = PoseStamped()
+        self.time = PoseStamped()
+        self.tilt_sub = self.create_subscription(Twist, '/cmd_vel', self.test_callback, 10)
+        self.i = 0
+        rclpy.spin(self.node)
+
+    def test_callback(self, msg):
+        if self.i == 0:
+            self.timestart.header.stamp = self.get_clock().now().to_msg()
+            self.timestart.pose.position.x = msg.position.x
+            self.timestart.pose.position.y = msg.position.y
+            self.timestart.pose.position.z = msg.position.z
+            ti = (float(self.timestart.header.stamp.sec)
+                  + float(self.timestart.header.stamp.nanosec) / 1000000000.0)
+        self.get_logger().info('goal pose: %f %f %f'
+                               % (msg.position.x, msg.position.y,
+                                  msg.position.z))
+        self.time.header.stamp = self.get_clock().now().to_msg()
+        self.time.pose.position.x = msg.position.x
+        self.time.pose.position.y = msg.position.y
+        self.time.pose.position.z = msg.position.z
+        tf = (float(self.time.header.stamp.sec)
+              + float(self.time.header.stamp.nanosec) / 1000000000.0)
+        self.i += 1
+        if (tf - ti) >= 1:
+            assert self.i == 100
+            self.i = 0
--- src/turtle_brick/test/math_test.py
+++ src/turtle_brick/test/math_test.py
@@ -1,2 +1,2 @@
-def test_math_sucsses():
-    assert 3**2 == 9
+def test_math_succss():
+    assert 3**2 == 8
